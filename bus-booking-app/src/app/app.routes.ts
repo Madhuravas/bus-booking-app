51796583bcec60c4864b699409ae2d161f7e4686
Madhuravas <madhuravasreddy1998@gmail.com>
2025-11-01 23:21:55 +0530

    Created required pages and components
    Signed-off-by: Madhuravas <madhuravasreddy1998@gmail.com>

--- bus-booking-app/src/app/app.routes.ts
+++ bus-booking-app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Search } from '../app/pages/search/search'
+import { SearchResult } from './pages/search-result/search-result';
+import { BookTicket } from './pages/book-ticket/book-ticket';
+import { MyBookings } from './pages/my-bookings/my-bookings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", redirectTo:"search", pathMatch: "full"},
+    {path:"search", component: Search},
+    {path:"search-result", component: SearchResult},
+    {path:"book-ticket", component: BookTicket},
+    {path:"my-bookings", component: MyBookings}
+];
